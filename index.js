d9d94275fa2bec07dde46e3e1d8e04643dd41575
Lesnevsky-Sergey <78030028+Sergey-Lesnevskiy@users.noreply.github.com>
2025-02-11 15:14:13 +0300

    fix: change project to deploy

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const quote = document.querySelector('.quatrain');
 const author = document.querySelector('.author');
+const work = document.querySelector('.work');
+const pathJSON = './assets/data.JSON';
 
 let i = 0;
 function dataAuthor(data) {
-  i >= data.length ? (i = 0) : quote.innerHTML = data[i].text; author.innerHTML = data[i].author ;
+  i >= data.length ? (i = 0) : quote.innerHTML = data[i].quatrain; author.innerHTML = data[i].author ;work.innerHTML = data[i].work ;
 }
 
 function getQuotes(path) {
   fetch(path)
     .then(res => res.json())
     .then(data => dataAuthor(data))
-    .catch(()=> quote.innerHTML = 'Не удалось загрузить стихотворение');
+    .catch(()=> {
+      quote.innerHTML = 'Не удалось загрузить стихотворение'
+      author.innerHTML = '';
+      work.innerHTML = '';
+    });
 }
 document.querySelector('.button').addEventListener('click', (e)=>{
   e.preventDefault();
-  getQuotes('./assets/rus.JSON');
+  getQuotes(pathJSON);
   i++;
   })
